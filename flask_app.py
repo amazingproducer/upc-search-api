ce6d21e07c1db70ac686b3aab5f65c25d2736ec7
amazingproducer <amazingproducer@gmail.com>
2020-05-06 02:29:01 +0000

    fix sorting call in usda query

--- flask_app.py
+++ flask_app.py
@@ -18,6 +18,7 @@ def lookup_uhtt(upc_string):
 def lookup_usda(upc_string):
     print(f"UPC REQUESTED FROM USDA: {upc_string}")
     upc_info = mongo.db.usda_upc.find({"gtin_upc": int(upc_string)}).sort({"available_date":-1}).limit(1)
+    print(upc_info)
     upc_name = mongo.db.usda_name.find_one_or_404({"fdc_id": upc_info["fdc_id"]})
 #    print(type(product_info))
     basic_info = {"code": upc_info["gtin_upc"], "product_name": upc_name["description"]}
