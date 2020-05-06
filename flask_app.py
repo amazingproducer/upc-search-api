0da70310021d11510a5bc9ba7f046d90fe3baa31
amazingproducer <amazingproducer@gmail.com>
2020-05-06 09:45:46 +0000

    add ugly consolidated result endpoint

--- flask_app.py
+++ flask_app.py
@@ -47,7 +47,6 @@ def lookup_off(upc_string):
 
 @app.route('/lookup/<upc_string>', methods=['GET'])
 def lookup(upc_string):
-    sources = 
     results = {"results": [lookup_off(upc_string).get_json(), lookup_usda(upc_string).get_json(), lookup_uhtt(upc_string).get_json()]}
     print(results)
     return results
