246e4666f5d0a3e50fd31ef10e6e2dfcc636230c
amazingproducer <amazingproducer@gmail.com>
2020-05-06 08:58:41 +0000

    add source name to usda result

--- flask_app.py
+++ flask_app.py
@@ -26,10 +26,10 @@ def lookup_usda(upc_string):
         upc_name = mongo.db.usda_name.find({"fdc_id": {"$in": fdc_ids}}).sort([("publication_date", -1)])[0]
         print(f'Found latest FDC entry: {upc_name["fdc_id"]}')
         upc_brand = mongo.db.usda_upc.find_one({"fdc_id": upc_name["fdc_id"]})["brand_owner"]
-        basic_info = {"source": "usda" {"code": upc_string, "product_name": f'{upc_brand} {upc_name["description"]}'}}
+        basic_info = {"source": "usda", "result": {"code": upc_string, "product_name": f'{upc_brand} {upc_name["description"]}'}}
         return jsonify(basic_info)
 #    abort(404)
-    return jsonify({"source": "usda" {"error": f"Entry not found.{upc_string}"}}), 404
+    return jsonify({"source": "usda", "result": {"error": f"Entry not found.{upc_string}"}}), 404
 
 @app.route('/off/<upc_string>', methods=['GET'])
 def lookup_off(upc_string):
