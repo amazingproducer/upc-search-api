7ec932fe0de8e63efadac64b5c48c481e1726726
amazingproducer <amazingproducer@gmail.com>
2020-05-21 02:35:53 +0000

    Add storability keyword matching for OpenFoodFacts route

--- flask_app.py
+++ flask_app.py
@@ -232,6 +232,7 @@ def lookup_off(upc_string):
 #    print(type(product_info))
     if product_info:
         basic_info = {"source": "OpenFoodFacts", "result": match_foodkeeper_product(" ".join(product_info['_keywords'])) }
+        print(basic_info)
         basic_info["result"]["code"] = str(upc_string)
         basic_info["result"]["product_name"] = product_info["product_name"]
 #    return mongo.db.product.PyMongo.find_one({"code": upc_string})
