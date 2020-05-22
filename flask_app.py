989534e7d04cb2d739aa9aec8e46039d42685fb4
amazingproducer <amazingproducer@gmail.com>
2020-05-22 14:49:12 +0000

    improve reliability of openfoodfacts category matching

--- flask_app.py
+++ flask_app.py
@@ -281,9 +281,9 @@ def lookup_off(upc_string):
                 c_q = " ".join(c_h[0:i])
                 c_r = match_foodkeeper_product(c_q)
                 if c_r and c_r[1] > c_s[1]:
-                    print(c_r)
+                    print(c_r, c_s)
                     c_s = c_r
-                    c_stor = get_storability(c_s[0], dsr=request.args.get('s', default = 'avg', type = str))
+                    c_stor = get_storability(c_r[0], dsr=request.args.get('s', default = 'avg', type = str))
                     print(c_stor)
             print(f"Product categories: {product_info['categories_hierarchy']}")
         else:
