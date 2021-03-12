d96d9e8fe8ed0c5c4c00c11eceb9792ff88d8427
amazingproducer <amazingproducer@gmail.com>
2021-03-12 23:11:16 +0000

    make a mess

--- data_update.py
+++ data_update.py
@@ -148,7 +148,7 @@ def upsert_off_entry(entry):
         """,
         (entry['source'], entry['source_item_id'], entry['upc'], entry['name'], entry['category'], entry['db_entry_date'], entry['source_item_submission_date'], entry['source_item_publication_date'], entry['serving_size_fulltext'])
         )
-#        print(db_cur.query.decode('utf-8'))
+        print(db_cur.query.decode('utf-8'))
     db_conn.close()
 
 start_time = dt.now()
