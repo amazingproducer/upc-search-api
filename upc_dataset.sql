bfa779ae547562ace290e934c0d0426c48eb2e96
amazingproducer <amazingproducer@gmail.com>
2021-03-16 23:15:29 +0000

    WIP #2 - simplify db constraints and indices

--- upc_dataset.sql
+++ upc_dataset.sql
@@ -17,7 +17,7 @@ CREATE TABLE product_info
     id                              serial PRIMARY KEY,
     source                          upc_data_source,
     source_item_id                  text,
-    upc                             varchar(14),
+    upc                             varchar(14) NOT NULL,
     name                            text NOT NULL,
     category                        text[],
     db_entry_date                   date NOT NULL,
@@ -30,8 +30,6 @@ CREATE TABLE product_info
     CONSTRAINT check_length CHECK (length(upc) >= 12),
     CONSTRAINT check_unique_composite UNIQUE (upc, source)
 );
-
-CREATE INDEX idx_product_info ON product_info (source, upc, source_item_publication_date);
 
 INSERT INTO dataset_source_meta (source_name, refresh_check_url, current_version_url) VALUES 
 (
